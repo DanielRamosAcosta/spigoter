259a44f89b3d3894dadaf17f5a677c3734bd9975
Daniel Ramos <danielramosacosta@hotmail.com>
2016-04-16 02:30:04 +0100

    Medio terminado la página

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
 $(function() {
-    $(".button-collapse").sideNav();
+	$(".button-collapse").sideNav();
+
+	var $root = $('html, body');
+	$('a').click(function() {
+		var href = $.attr(this, 'href');
+		$root.animate({
+			scrollTop: $(href).offset().top
+		}, 300, function () {
+			window.location.hash = href;
+		});
+		return false;
+	});
+
+	var maxheight = 0;
+	if( $("#card-compile").height() > maxheight )
+		maxheight = $("#card-compile").height();
+
+	if( $("#card-start").height() > maxheight )
+		maxheight = $("#card-start").height();
+
+	if( $("#card-update").height() > maxheight )
+		maxheight = $("#card-update").height();
+
+	$("#card-compile").height(maxheight);
+	$("#card-start").height(maxheight);
+	$("#card-update").height(maxheight);
+
+	$.get("github.com/DanielRamosAcosta/spigoter/releases/latest", function(data) {
+		console.log(data);
+	});
 });
